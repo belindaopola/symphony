d5fa88433976c2946d7924d056efa7f5f6f4e820
amandasuzzanne <amandasuzanne12@gmail.com>
2024-05-15 16:26:30 +0300

    Update form

--- add-admin.php
+++ add-admin.php
@@ -1,105 +1,101 @@
 <?php include('partials/menu.php'); ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
 <div class="main-content">
     <div class="wrapper">
-        <h1>Add Admin</h1>
-
-        <br><br>
+        <h1 class="row mb-4">Add Admin</h1>
 
         <?php 
-            if(isset($_SESSION['add'])) //Checking whether the SEssion is Set of Not
+            if(isset($_SESSION['add'])) // Checking whether the Session is Set of Not
             {
-                echo $_SESSION['add']; //Display the SEssion Message if SEt
-                unset($_SESSION['add']); //Remove Session Message
+                echo $_SESSION['add']; // Display the Session Message if Set
+                unset($_SESSION['add']); // Remove Session Message
             }
         ?>
 
         <form action="" method="POST">
-
-            <table class="tbl-30">
-                <tr>
-                    <td>Full Name: </td>
-                    <td>
-                        <input type="text" name="full_name" placeholder="Enter Your Name">
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>Username: </td>
-                    <td>
-                        <input type="text" name="username" placeholder="Your Username">
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>Password: </td>
-                    <td>
-                        <input type="password" name="password" placeholder="Your Password">
-                    </td>
-                </tr>
-
-                <tr>
-                    <td colspan="2">
-                        <input type="submit" name="submit" value="Add Admin" class="btn-secondary">
-                    </td>
-                </tr>
-
-            </table>
-
+        <div class="row mb-4">
+            <label for="inputFullName" class="col-sm-1 col-form-label">Full Name:</label>
+                <div class="col-sm-3">
+                <input type="text" id="full-name" name="full_name" placeholder="Enter Name" class="form-control">
+                </div>
+            </div>
+            <div class="row mb-4">
+            <label for="InputUsername" class="col-sm-1 col-form-label">Username:</label>
+                <div class="col-sm-3">
+                <input type="text" id="username" name="username" placeholder="Enter Username" class="form-control">
+                </div>
+            </div>
+            <div class="row mb-4">
+            <label for="inputPassword" class="col-sm-1 col-form-label">Password:</label>
+                <div class="col-sm-3">
+                <input type="password" id="password" name="password" placeholder="Enter Password" class="form-control" >
+                </div>
+            </div>
+            <input type="submit" name="submit" value="Add Admin" class="btn btn-primary col-sm-1">
         </form>
-
 
     </div>
 </div>
 
 <?php include('partials/footer.php'); ?>
 
-
 <?php 
-    //Process the Value from Form and Save it in Database
+    // Process the Value from Form and Save it in Database
 
-    //Check whether the submit button is clicked or not
+    // Check whether the submit button is clicked or not
 
     if(isset($_POST['submit']))
     {
         // Button Clicked
         //echo "Button Clicked";
 
-        //1. Get the Data from form
+        // 1. Get the Data from form
         $full_name = $_POST['full_name'];
         $username = $_POST['username'];
         $password = md5($_POST['password']); //Password Encryption with MD5
 
-        //2. SQL Query to Save the data into database
+        // 2. SQL Query to Save the data into database
         $sql = "INSERT INTO tbl_admin SET 
             full_name='$full_name',
             username='$username',
             password='$password'
         ";
  
-        //3. Executing Query and Saving Data into Datbase
+        // 3. Executing Query and Saving Data into Datbase
         $res = mysqli_query($conn, $sql) or die(mysqli_error());
 
-        //4. Check whether the (Query is Executed) data is inserted or not and display appropriate message
+        // 4. Check whether the (Query is Executed) data is inserted or not and display appropriate message
         if($res==TRUE)
         {
-            //Data Inserted
+            // Data Inserted
             //echo "Data Inserted";
-            //Create a Session Variable to Display Message
+            // Create a Session Variable to Display Message
             $_SESSION['add'] = "<div class='success'>Admin Added Successfully.</div>";
-            //Redirect Page to Manage Admin
+            // Redirect Page to Manage Admin
             header("location:".SITEURL.'manage-admin.php');
         }
         else
         {
-            //FAiled to Insert DAta
+            // Failed to Insert DAta
             //echo "Faile to Insert Data";
-            //Create a Session Variable to Display Message
+            // Create a Session Variable to Display Message
             $_SESSION['add'] = "<div class='error'>Failed to Add Admin.</div>";
-            //Redirect Page to Add Admin
+            // Redirect Page to Add Admin
             header("location:".SITEURL.'add-admin.php');
         }
 
     }
     
 ?>
+</body>
+</html>
+
