c402050dab229d04c158d8d38dfd99593e23cedc
amandasuzzanne <amandasuzanne12@gmail.com>
2024-05-15 14:24:23 +0300

    Update login form

--- login.php
+++ login.php
@@ -11,8 +11,7 @@
         
         <div class="login">
             <h1 class="text-center">Login</h1>
-            <br><br>
-
+          
             <?php 
                 if(isset($_SESSION['login']))
                 {
@@ -26,22 +25,26 @@
                     unset($_SESSION['no-login-message']);
                 }
             ?>
-            <br><br>
 
-            <!-- Login Form Starts HEre -->
+            <!-- Login Form Starts Here -->
             <form action="" method="POST" class="text-center">
-            Username: <br>
-            <input type="text" name="username" placeholder="Enter Username"><br><br>
-
-            Password: <br>
-            <input type="password" name="password" placeholder="Enter Password"><br><br>
-
-            <input type="submit" name="submit" value="Login" class="btn-primary">
-            <br><br>
+            <div class="row mb-4">
+               <label for="inputUsername" class="col-sm-3 col-form-label">Username:</label>
+               <div class="col-sm-6">
+                    <input type="text" class="form-control"  name="username" required>
+               </div>
+            </div>
+            <div class="row mb-4">
+               <label for="inputPassword" class="col-sm-3 col-form-label">Password:</label>
+               <div class="col-sm-6">
+                    <input type="password" class="form-control" name="password" required>
+               </div>
+            </div>
+            <button type="submit" class="btn btn-primary col-sm-3" name="login" style="margin-left:40";>Login</button> 
             </form>
-            <!-- Login Form Ends HEre -->
+            <!-- Login Form Ends Here -->
 
-            <p class="text-center">Created By - <a href="www.maradesigns.co.ke">Belinda Opola</a></p>
+            <p class="text-center" style="margin-top:40">Created By - <a href="www.maradesigns.co.ke">Belinda Opola</a></p>
         </div>
 
     </body>
@@ -49,11 +52,11 @@
 
 <?php 
 
-    //CHeck whether the Submit Button is Clicked or NOt
+    // Check whether the Submit Button is Clicked or NOt
     if(isset($_POST['submit']))
     {
-        //Process for Login
-        //1. Get the Data from Login form
+        // Process for Login
+        // 1. Get the Data from Login form
         // $username = $_POST['username'];
         // $password = md5($_POST['password']);
         $username = mysqli_real_escape_string($conn, $_POST['username']);
@@ -61,29 +64,29 @@
         $raw_password = md5($_POST['password']);
         $password = mysqli_real_escape_string($conn, $raw_password);
 
-        //2. SQL to check whether the user with username and password exists or not
+        // 2. SQL to check whether the user with username and password exists or not
         $sql = "SELECT * FROM tbl_admin WHERE username='$username' AND password='$password'";
 
-        //3. Execute the Query
+        // 3. Execute the Query
         $res = mysqli_query($conn, $sql);
 
-        //4. COunt rows to check whether the user exists or not
+        // 4. Count rows to check whether the user exists or not
         $count = mysqli_num_rows($res);
 
         if($count==1)
         {
-            //User AVailable and Login Success
+            // User AVailable and Login Success
             $_SESSION['login'] = "<div class='success'>Login Successful.</div>";
             $_SESSION['user'] = $username; //TO check whether the user is logged in or not and logout will unset it
 
-            //REdirect to HOme Page/Dashboard
+            // Redirect to HOme Page/Dashboard
             header('location:'.SITEURL.'/');
         }
         else
         {
-            //User not Available and Login FAil
+            // User not Available and Login FAil
             $_SESSION['login'] = "<div class='error text-center'>Username or Password did not match.</div>";
-            //REdirect to HOme Page/Dashboard
+            // Redirect to HOme Page/Dashboard
             header('location:'.SITEURL.'login.php');
         }
 
