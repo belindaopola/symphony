6fbdde45bef89bfac2282d6669498cbca7c6d133
amandasuzzanne <amandasuzanne12@gmail.com>
2024-05-15 16:29:24 +0300

    Update buttons styling

--- manage-request.php
+++ manage-request.php
@@ -1,159 +1,166 @@
 <?php include('partials/menu.php'); ?>
 
-<div class="main-content">
-    <div class="wrapper">
-        <h1>Manage request</h1>
-
-                <br /><br /><br />
-
-                <?php 
-                    if(isset($_SESSION['update']))
-                    {
-                        echo $_SESSION['add']; //Displaying Session Message
-                        unset($_SESSION['add']); //REmoving Session Message
-                    }
-
-                    if(isset($_SESSION['delete']))
-                    {
-                        echo $_SESSION['delete'];
-                        unset($_SESSION['delete']);
-                    }
-                    
-                    if(isset($_SESSION['update']))
-                    {
-                        echo $_SESSION['update'];
-                        unset($_SESSION['update']);
-                    }
-                ?>
-                <br/><br/><br>/
-
-                <!-- Button to Add Admin -->
-                <a href="add-request.php" class="btn-primary">New CSR</a>
-
-                <br /><br /><br />
-
-                <table class="tbl-full">
-                    <tr>
-                        <th>CSR Ref.</th>
-                        <th>Request Date</th>
-                        <th>Customer Name</th>
-                        <th>Description</th>
-                        <th>Quotation</th>
-                        <th>Customer PO</th>
-                        <th>Costing Sheet</th>
-                        <th>Currency</th>
-                        <th>Amount</th>
-                        <th>VAT</th>
-                        <th>Total</th>
-                        <th>Status</th>
-                        <th>Salesperson</th>
-                        <th>Actions</th>
-                    </tr>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Manage Request</title>
+</head>
+<body>
+    <div class="main-content">
+        <div class="wrapper" style="width:100%">
+            <h1 class="row mb-4">Manage Request</h1>
+    
                     <?php 
-                        //Get all the requests from database
-                        $sql = "SELECT * FROM tbl_request"; // DIsplay the Latest request at First
-                        //Execute Query
-                        $res = mysqli_query($conn, $sql);
-                        //Count the Rows
-                        $count = mysqli_num_rows($res);
-
-                        $sn = 1; //Create a Serial Number and set its initail value as 1
-
-                        if($count>0)
+                        if(isset($_SESSION['update']))
                         {
-                            //request Available
-                            while($row=mysqli_fetch_assoc($res))
-                            {
-                                //Get all the request details
-                                $id = $row['id'];
-                                $request_date = $row['request_date'];
-                                $customer_name = $row['customer_name'];
-                                $description = $row['description'];
-                                $quotation = $row['quotation'];
-                                $customer_po = $row['customer_po'];
-                                $costing_sheet = $row['costing_sheet'];
-                                $currency = $row['currency'];
-                                $price = $row['price'];
-                                $vat = $row['vat'];
-                                $total = $row['total'];
-                                $status = $row['status'];
-                                $salesperson = $row['sales_person'];
-                                
-                                ?>
-
-                                    <tr>
-                                        <td><?php echo "TS" . date("Y") . "/" . sprintf('%03d', $sn++); ?></td>
-                                        <td><?php echo $request_date; ?></td>
-                                        <td><?php echo $customer_name; ?></td>
-                                        <td><?php echo $description; ?></td>
-                                        <td>
-                                            <?php
-                                                // Check whether quotation attachment is available or not
-                                                if ($quotation != "") {
-                                                    // Display quotation image
-                                                    ?>
-                                                    <a href="<?php echo SITEURL; ?>uploads/quotation/<?php echo $quotation; ?>" target="_blank">View Quotation</a>
-                                                <?php
-                                                } else {
-                                                    echo "<div class='error'>Please attach quotation.</div>";
-                                                }
-                                            ?>
-                                        </td>
-
-                                        <td>
-                                            <?php
-                                                // Check whether purchase order attachment is available or not
-                                                if ($customer_po != "") {
-                                                    // Display purchase order image
-                                                    ?>
-                                                    <a href="<?php echo SITEURL; ?>uploads/po/<?php echo $customer_po; ?>" target="_blank">View Purchase Order</a>
-                                                <?php
-                                                } else {
-                                                    echo "<div class='error'>Please attach purchase order.</div>";
-                                                }
-                                            ?>
-                                        </td>
-
-                                        <td>
-                                            <?php
-                                                // Check whether costing sheet attachment is available or not
-                                                if ($costing_sheet != "") {
-                                                    // Display costing sheet image
-                                                    ?>
-                                                    <a href="<?php echo SITEURL; ?>uploads/costing/<?php echo $costing_sheet; ?>" target="_blank">View Costing Sheet</a>
-                                                <?php
-                                                } else {
-                                                    echo "<div class='error'>Please attach costing sheet.</div>";
-                                                }
-                                            ?>
-                                        </td>
-                                        <td><?php echo $currency; ?></td>
-                                        <td><?php echo $price; ?></td>
-                                        <td><?php echo $vat; ?></td>
-                                        <td><?php echo $total; ?></td>
-                                        <td><?php echo $status; ?></td>
-                                        <td><?php echo $salesperson; ?></td>
-                                        <td>
-                                            <a href="<?php echo SITEURL; ?>update-request.php?id=<?php echo $id; ?>" class="btn-secondary">Update request</a>
-                                        </td>
-                                    </tr>
-
-                                <?php
-
-                            }
+                            echo $_SESSION['add']; // Displaying Session Message
+                            unset($_SESSION['add']); // Removing Session Message
                         }
-                        else
+    
+                        if(isset($_SESSION['delete']))
                         {
-                            //Request not Available
-                            echo "<tr><td colspan='12' class='error'>No request available</td></tr>";
+                            echo $_SESSION['delete'];
+                            unset($_SESSION['delete']);
+                        }
+                        
+                        if(isset($_SESSION['update']))
+                        {
+                            echo $_SESSION['update'];
+                            unset($_SESSION['update']);
                         }
                     ?>
-
- 
-                </table>
+    
+                    <!-- Button to Add Admin -->
+                    <a href="add-request.php" class="btn btn-primary col-sm-1 row mb-4">New CSR</a>
+        
+                    <table class="tbl-full">
+                        <tr>
+                            <th>CSR Ref.</th>
+                            <th>Request Date</th>
+                            <th>Customer Name</th>
+                            <th>Description</th>
+                            <th>Quotation</th>
+                            <th>Customer PO</th>
+                            <th>Costing Sheet</th>
+                            <th>Currency</th>
+                            <th>Amount</th>
+                            <th>VAT</th>
+                            <th>Total</th>
+                            <th>Status</th>
+                            <th>Salesperson</th>
+                            <th>Actions</th>
+                        </tr>
+    
+                        <?php 
+                            //Get all the requests from database
+                            $sql = "SELECT * FROM tbl_request"; // DIsplay the Latest request at First
+                            //Execute Query
+                            $res = mysqli_query($conn, $sql);
+                            //Count the Rows
+                            $count = mysqli_num_rows($res);
+    
+                            $sn = 1; //Create a Serial Number and set its initail value as 1
+    
+                            if($count>0)
+                            {
+                                //request Available
+                                while($row=mysqli_fetch_assoc($res))
+                                {
+                                    //Get all the request details
+                                    $id = $row['id'];
+                                    $request_date = $row['request_date'];
+                                    $customer_name = $row['customer_name'];
+                                    $description = $row['description'];
+                                    $quotation = $row['quotation'];
+                                    $customer_po = $row['customer_po'];
+                                    $costing_sheet = $row['costing_sheet'];
+                                    $currency = $row['currency'];
+                                    $price = $row['price'];
+                                    $vat = $row['vat'];
+                                    $total = $row['total'];
+                                    $status = $row['status'];
+                                    $salesperson = $row['sales_person'];
+                                    
+                                    ?>
+    
+                                        <tr>
+                                            <td><?php echo "TS" . date("Y") . "/" . sprintf('%03d', $sn++); ?></td>
+                                            <td><?php echo $request_date; ?></td>
+                                            <td><?php echo $customer_name; ?></td>
+                                            <td><?php echo $description; ?></td>
+                                            <td>
+                                                <?php
+                                                    // Check whether quotation attachment is available or not
+                                                    if ($quotation != "") {
+                                                        // Display quotation image
+                                                        ?>
+                                                        <a href="<?php echo SITEURL; ?>uploads/quotation/<?php echo $quotation; ?>" target="_blank">View Quotation</a>
+                                                    <?php
+                                                    } else {
+                                                        echo "<div class='error'>Please attach quotation.</div>";
+                                                    }
+                                                ?>
+                                            </td>
+    
+                                            <td>
+                                                <?php
+                                                    // Check whether purchase order attachment is available or not
+                                                    if ($customer_po != "") {
+                                                        // Display purchase order image
+                                                        ?>
+                                                        <a href="<?php echo SITEURL; ?>uploads/po/<?php echo $customer_po; ?>" target="_blank">View Purchase Order</a>
+                                                    <?php
+                                                    } else {
+                                                        echo "<div class='error'>Please attach purchase order.</div>";
+                                                    }
+                                                ?>
+                                            </td>
+    
+                                            <td>
+                                                <?php
+                                                    // Check whether costing sheet attachment is available or not
+                                                    if ($costing_sheet != "") {
+                                                        // Display costing sheet image
+                                                        ?>
+                                                        <a href="<?php echo SITEURL; ?>uploads/costing/<?php echo $costing_sheet; ?>" target="_blank">View Costing Sheet</a>
+                                                    <?php
+                                                    } else {
+                                                        echo "<div class='error'>Please attach costing sheet.</div>";
+                                                    }
+                                                ?>
+                                            </td>
+                                            <td><?php echo $currency; ?></td>
+                                            <td><?php echo $price; ?></td>
+                                            <td><?php echo $vat; ?></td>
+                                            <td><?php echo $total; ?></td>
+                                            <td><?php echo $status; ?></td>
+                                            <td><?php echo $salesperson; ?></td>
+                                            <td>
+                                                <a href="<?php echo SITEURL; ?>update-request.php?id=<?php echo $id; ?>" class="btn btn-secondary col-sm-2.5">Update request</a>
+                                            </td>
+                                        </tr>
+    
+                                    <?php
+    
+                                }
+                            }
+                            else
+                            {
+                                //Request not Available
+                                echo "<tr><td colspan='12' class='error'>No request available</td></tr>";
+                            }
+                        ?>
+    
+     
+                    </table>
+        </div>
+        
     </div>
     
-</div>
-
-<?php include('partials/footer.php'); ?>
+    <?php include('partials/footer.php'); ?>
+    
+</body>
+</html>
