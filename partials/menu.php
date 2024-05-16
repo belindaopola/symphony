b22010ebefc015ca79da28a08f7526622f92f2d8
amandasuzzanne <amandasuzanne12@gmail.com>
2024-05-16 11:04:41 +0300

    Add bootstrap link to header

--- partials/menu.php
+++ partials/menu.php
@@ -9,7 +9,7 @@
 <html>
     <head>
         <title>Service Requests - Home Page</title>
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="css/admin.css">
     </head>
     
