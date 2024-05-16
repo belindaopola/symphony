1b9efe973b577e05021889040a8ad3e2aad74afb
Belinda Opola <belindaopola@gmail.com>
2024-05-16 12:27:44 +0300

    Formatted menu.php

--- partials/menu.php
+++ partials/menu.php
@@ -1,31 +1,28 @@
 <?php 
-
-    include('config/constants.php'); 
-    include('login-check.php');
-
+include('config/constants.php'); 
+include('login-check.php');
 ?>
 
-
 <html>
-    <head>
-        <title>Service Requests - Home Page</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/admin.css">
-    </head>
-    
-    <body>
-        <!-- Menu Section Starts -->
-        <div class="menu text-center">
-            <div class="wrapper">
-                <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="manage-admin.php">Admin</a></li>
-                    <li><a href="manage-section.php">Section</a></li>
-                    <li><a href="manage-customer.php">Customers</a></li>
-                    <li><a href="manage-product.php">Product</a></li>
-                    <li><a href="manage-request.php">Requests</a></li>
-                    <li><a href="logout.php">Logout</a></li>
-                </ul>
-            </div>
+<head>
+    <title>Service Requests - Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/admin.css">
+</head>
+
+<body>
+    <!-- Menu Section Starts -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-section.php">Section</a></li>
+                <li><a href="manage-customer.php">Customers</a></li>
+                <li><a href="manage-product.php">Product</a></li>
+                <li><a href="manage-request.php">Requests</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
         </div>
-        <!-- Menu Section Ends -->
+    </div>
+    <!-- Menu Section Ends -->
